21b320a699fe4b2d5a5955e7eadd3c9d87a28baf
Bruno Candia <brunocandia@gmail.com>
2026-08-08 16:58:57 -0300

    Rollback Updated Terraform Provider

--- infra/provider.tf
+++ infra/provider.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     azurerm = {
         source = "hashicorp/azurerm"
-        version = "5.0.0"
-        # version = "4.46.0"
+        # version = "5.0.0" // Use dotnet_version = "10.0"
+        version = "4.46.0" // Use dotnet_version = "9.0"
         # version = "~> 3.0"
     }
   }
