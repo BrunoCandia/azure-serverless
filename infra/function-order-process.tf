095cee7409de5822183e197e782ca4bcfb0278d8
Bruno Candia <brunocandia@gmail.com>
2026-08-08 17:00:57 -0300

    Rollback Updated Terraform Provider

--- infra/function-order-process.tf
+++ infra/function-order-process.tf
@@ -9,7 +9,8 @@ resource "azurerm_linux_function_app" "order-process-func" {
 
   site_config {
     application_stack {
-      dotnet_version = "10.0"
+      # dotnet_version = "10.0"
+      dotnet_version = "9.0"
       use_dotnet_isolated_runtime = true      
     }
   }
