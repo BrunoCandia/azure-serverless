2228a70b21070169c58f9dc7a79edd7915df25bf
Bruno Candia <brunocandia@gmail.com>
2026-08-10 12:17:13 -0300

    Add App Insights for Order Tracker Functio and add CosmosDB

--- infra/function-order-tracker-logger.tf
+++ infra/function-order-tracker-logger.tf
@@ -15,7 +15,18 @@ resource "azurerm_linux_function_app" "order_tracker_logger_func" {
 
   app_settings = {
     FUNCTIONS_WORKER_RUNTIME = "node"
-    AzureWebJobsStorage = azurerm_storage_account.main.primary_connection_string        
-  }
-  
+    AzureWebJobsStorage = azurerm_storage_account.main.primary_connection_string
+
+    APPLICATIONINSIGHTS_CONNECTION_STRING = azurerm_application_insights.order_tracker_logger_insights.connection_string
+    ApplicationInsightsAgent_EXTENSION_VERSION = "~3"
+  }    
+}
+
+resource "azurerm_application_insights" "order_tracker_logger_insights" {
+  name                = "${var.project_name}-order-tracker-logger-${var.environment}-ai"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  application_type    = "other"
+
+  workspace_id        = azurerm_log_analytics_workspace.main.id  
 }
