294d06f3aa84edc5c3b37730d4bacac5204b5de1
Bruno Candia <brunocandia@gmail.com>
2026-08-10 12:36:18 -0300

    Add CosmosDB database and table

--- infra/cosmosdb.tf
+++ infra/cosmosdb.tf
@@ -1,3 +1,5 @@
+# Database Server
+
 resource "azurerm_cosmosdb_account" "main" {
   name = "${var.project_name}-${var.environment}-cosmosdb100"
   location = azurerm_resource_group.main.location
@@ -24,4 +26,21 @@ resource "azurerm_cosmosdb_account" "main" {
     project = var.project_name
     "course" = "serverless"
   }
+}
+
+# Database Engine
+resource "azurerm_cosmosdb_sql_database" "orders_db" {
+  name                = "orders-db"
+  resource_group_name = azurerm_resource_group.main.name
+  account_name        = azurerm_cosmosdb_account.main.name
+}
+
+# Database Table
+resource "azurerm_cosmosdb_sql_container" "orders" {
+  name                = "orders"
+  resource_group_name = azurerm_resource_group.main.name
+  account_name        = azurerm_cosmosdb_account.main.name
+  database_name       = azurerm_cosmosdb_sql_database.orders_db.name
+
+  partition_key_paths  = ["/id"]
 }
