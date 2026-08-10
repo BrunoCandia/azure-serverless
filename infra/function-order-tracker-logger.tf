4273363eee0fdfebbe74fb1bd3b973769cf5f55a
Bruno Candia <brunocandia@gmail.com>
2026-08-10 11:41:03 -0300

    Add infra for Order Tracker Logger Function

---
+++ infra/function-order-tracker-logger.tf
@@ -0,0 +1,21 @@
+resource "azurerm_linux_function_app" "order_tracker_logger_func" {
+  name                       = "${var.project_name}-order-tracker-logger-${var.environment}-func100"
+  resource_group_name        = azurerm_resource_group.main.name
+  location                   = azurerm_resource_group.main.location
+  service_plan_id            = azurerm_app_service_plan.my_plan.id
+
+  storage_account_name       = azurerm_storage_account.main.name
+  storage_account_access_key = azurerm_storage_account.main.primary_access_key
+
+  site_config {
+    application_stack {
+      node_version = "20"
+    }
+  }
+
+  app_settings = {
+    FUNCTIONS_WORKER_RUNTIME = "node"
+    AzureWebJobsStorage = azurerm_storage_account.main.primary_connection_string        
+  }
+  
+}
